d212e5424ebf39e3afa9d8f486aa702b1051bfb0
Alexandr Kondratskii <aleksandrkondratskii@gmail.com>
2025-07-06 13:16:01 +0200

    fix broken injection in click handler

--- src/background/handleError.ts
+++
@@ -1,7 +0,0 @@
-export const handleError = (error: unknown): void => {
-  if (error instanceof Error) {
-    console.error(`WitFast Error: ${error.message}`);
-  } else {
-    console.error(`WitFast Error: ${String(error)}`);
-  }
-};
